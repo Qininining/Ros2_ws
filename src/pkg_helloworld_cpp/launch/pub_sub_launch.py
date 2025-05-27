e2bc8c2326c7d19333d91a4933ebf4336a7da59f
Qininining <204747508@qq.com>
2025-05-27 22:20:55 +0800

    添加简单的发布者和订阅者节点，并更新CMakeLists.txt和启动文件

---
+++ src/pkg_helloworld_cpp/launch/pub_sub_launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='pkg_helloworld_cpp',
+            executable='simple_publisher',
+            name='simple_publisher',
+            output='screen'
+        ),
+        Node(
+            package='pkg_helloworld_cpp',
+            executable='simple_subscriber',
+            name='simple_subscriber',
+            output='screen'
+        )
+    ])
